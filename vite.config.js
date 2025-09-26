f41aa3445f66fbebf915b01039a99c5555335c7f
Ale <amazzuca@hotmail.com>
2025-09-26 14:45:13 -0300

    Configuración final para despliegue en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Reemplaza 'mi-app-flujo-de-caja' con el nombre de tu repo.
-  base: '/AppCashFlow/', 
-  plugins: [
-    react(),
-  ],
-  // El bloque 'css' se ha eliminado para que no entre en conflicto
-  // con el archivo postcss.config.js
+  plugins: [react()],
+  base: '/AppCashFlow/'
 })
-
